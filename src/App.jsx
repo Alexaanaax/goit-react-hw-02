b5f3ca0f47a0db1f29bc50459559c645e44458af
Alexaanaax <149320517+Alexaanaax@users.noreply.github.com>
2024-05-05 16:13:15 +0200

    add

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+import Description from "./components/Description";
+import Options from "./components/Options";
+import Feedback from "./components/Feedback";
+
+import reviews from "./reviews.json";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
